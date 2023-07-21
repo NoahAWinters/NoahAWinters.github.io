c60ce01cbee30d58b92c08a2a71d0e788e6bf81a
noah winters <noahwinters9898@gmail.com>
2023-07-21 17:20:07 -0400

    fix to responsivness issues

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('navList')
 
+let width = screen.width;
 
 function toggleNavButton() {
     navList.classList.toggle('show')
+}
+
+function showText() {
+
 }
 
 hamburgerButton.addEventListener('click', toggleNavButton)
