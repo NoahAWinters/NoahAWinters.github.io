12458bb18a7a1965773da798c8f7d5bcde8b05cd
noah winters <noahwinters9898@gmail.com>
2023-07-24 14:19:07 -0400

    Added responsiveness. Fixed NavBar resize issue

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('navList')
-
+const navbarShow = true;
 let width = screen.width;
+
 
 function toggleNavButton() {
     navList.classList.toggle('show')
+    navbarShow = !navbarShow
 }
 
-function showText() {
+function resetNavbar() {
+    if (width >= 1000 && navbarShow) {
+        navList.classList.remove('show')
+        navbarShow = false
+    }
 
 }
+
 
 hamburgerButton.addEventListener('click', toggleNavButton)
+window.addEventListener('resize', resetNavbar)
