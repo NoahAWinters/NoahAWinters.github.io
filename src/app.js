34aa886522435a1b01e56664a7077202be4e5240
noah winters <noahwinters9898@gmail.com>
2023-07-24 17:29:29 -0400

    Added contact form

--- src/app.js
+++ src/app.js
@@ -1,22 +1,37 @@
 const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('navList')
 const navbarShow = true;
-let width = screen.width;
+
+hamburgerButton.addEventListener('click', toggleNavButton)
+window.addEventListener('resize', resetNavbar)
 
 
+//Nav Bar
 function toggleNavButton() {
     navList.classList.toggle('show')
     navbarShow = !navbarShow
 }
 
 function resetNavbar() {
+    let width = screen.width;
+
     if (width >= 1000 && navbarShow) {
         navList.classList.remove('show')
         navbarShow = false
     }
-
 }
 
-
-hamburgerButton.addEventListener('click', toggleNavButton)
-window.addEventListener('resize', resetNavbar)
+//Contact Form https://www.youtube.com/watch?v=sGQSz22U8VM
+function sendEmail() {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "username",
+        Password: "password",
+        To: 'them@website.com',
+        From: "you@isp.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
