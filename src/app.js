90d45085ff739b2ac5b6333abb264d976561316c
noah winters <noahwinters9898@gmail.com>
2024-03-06 14:34:06 -0500

    New Nav Bar

--- src/app.js
+++ src/app.js
@@ -1,32 +1,9 @@
-const hamburgerButton = document.getElementById('hamburger');
-const navList = document.getElementById('navList');
-const nav = document.getElementById('navbar');
-const navbarShow = true;
-
-hamburgerButton.addEventListener('click', function () {
-    navList.classList.toggle('show');
-    hamburgerButton.classList.toggle('show');
-    nav.classList.toggle('stayGreen');
-    navbarShow = !navbarShow;
-
-});
-window.addEventListener('resize', resetNavbar);
-
-
-//Nav Bar
-// function toggleNavButton() {
-//     navList.classList.toggle('show');
-//     hamburgerButton.classList.toggle('show');
-//     navbarShow = !navbarShow;
-// }
-
-function resetNavbar() {
-    let width = screen.width;
-
-    if (width >= 1000 && navbarShow) {
-        navList.classList.remove('show');
-        hamburgerButton.classList.remove('show');
-        nav.classList.remove('stayGreen');
-        navbarShow = false;
+let navbar = document.querySelector('.navbar')
+document.addEventListener('scroll', () => {
+    if (window.top.scrollY > 19) {
+        navbar.classList.add('scroll')
+    } else {
+        navbar.classList.remove('scroll')
+        navbar.style.transition = '.5s ease ';
     }
-}
+})
